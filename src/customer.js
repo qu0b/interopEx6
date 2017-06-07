38b557fe3b08a7e368565274687d814a9a1e0611
Stefan Starflinger <st3f4n.s@googlemail.com>
2017-06-07 21:00:01 +0200

    message

--- src/customer.js
+++ src/customer.js
@@ -25,61 +25,6 @@ var genOrder = () => {
 
 app.use('/*', (req, res, next) => {
 	console.log('call to customer');
-
-	let logExact ={
-      file: "customer.js",
-      contentType: req.get('Content-Type'),
-      acceptEncoding: req.get('Accept-Encoding'),
-      accept: req.get('Accept'),
-      userAgent: req.get('User-Agent'),
-      contentLength: req.get('Content-Length'),
-      Host: req.get('Host'),
-      method: req.method,
-      url: req.url,
-      param: req.params,
-      body: req.body,
-      timestamp: Math.floor(new Date() / 1000),
-      cpee: {
-        cpeeBase: req.get('Cpee_base'),
-        cpeeInstance: req.get('Cpee_instance'),
-        cpeeCallback: req.get('Cpee_callback'),
-        cpeeActivity: req.get('Cpee_activity'),
-        cpeeLabel: req.get('Cpee_label'),
-        cpeeAttrInfo: req.get('Cpee_attr_info'),
-        cpeeAttrModeltype: req.get('Cpee_attr_modeltype')
-      },
-      active: true,
-      progress: 0
-  }
-
-  for (var fav in logExact) {
-    if (logExact.hasOwnProperty(fav)) {
-      if (fav=='param') {
-        for (var par in logExact[fav]) {
-          if (fav.hasOwnProperty(par)) {
-            if(logExact[fav][par]==='favicon.ico')
-            {
-              favicon=false
-            }
-          }
-        }
-
-      }
-    }
-  }
-
-  if(favicon){
-    // fs.appendFile('log.json', JSON.stringify(log, null, '  ') , err => {
-    // if (err) throw err;
-    // });
-    fs.appendFile('./log/logCorrelator.json', JSON.stringify(logExact, null, '  ') , err => {
-    if (err) throw err;
-    });
-  }
-
-
-
-
   next()
 })
 
